1a12227e94670dc8e18582faa1ca28a32433cccf
Flavio Santos <flaviovansantos@gmail.com>
2020-09-18 15:00:35 -0300

    DTO, Service

--- backend/dspesquisa/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
+++ backend/dspesquisa/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dspesquisa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dspesquisa.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
+	
 
 }
